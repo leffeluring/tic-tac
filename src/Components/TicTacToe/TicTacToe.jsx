5282a0a7ee14f95999962d92a53d805d062a198f
Rasmus <hello@leifsgard.com>
2024-02-07 00:35:14 +0100

    Friday

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import './TicTacToe.css';
 import circle_icon from '../Assets/jocke.png';
-import cross_icon from '../Assets/cross.png';
+import cross_icon from '../Assets/beer.png';
 
 
 function Square({ value, onClick }) {
     let squareClass = "square";
     let squareContent = value;
-    if (value === 'X') {
+    if (value === 'Øl') {
         squareClass += " x-square";
-    } else if (value === 'O') {
+    } else if (value === 'Jocke') {
         squareClass += " o-square";
     }
     return (
@@ -27,7 +27,7 @@ function TicTacToe() {
   const handleClick = (index) => {
     if (winner || squares[index]) return;
     const newSquares = [...squares];
-    newSquares[index] = xIsNext ? 'X' : 'O';
+    newSquares[index] = xIsNext ? 'Øl' : 'Jocke';
     setSquares(newSquares);
     setXIsNext(!xIsNext);
     setWinner(calculateWinner(newSquares));
@@ -47,7 +47,7 @@ function TicTacToe() {
     }, 500); // Delay AI move for better user experience
   }
 
-  const status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? 'X' : 'O'}`;
+  const status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? 'Øl' : 'Jocke'}`;
 
   return (
     <div className='container'>
